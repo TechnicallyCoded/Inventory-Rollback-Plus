debcc24f05ca598e4c9c421035c7929784ace670
danjono <12578450+danjono@users.noreply.github.com>
2017-10-15 22:10:09 +0100

    Removed old redundant code.

--- src/main/java/me/danjono/inventoryrollback/InventoryRollback.java
+++ src/main/java/me/danjono/inventoryrollback/InventoryRollback.java
@@ -64,14 +64,6 @@ public class InventoryRollback extends JavaPlugin {
 		v1_11_R1,
 		v1_12_R1
 	}
-	
-	private enum v18to19 {
-		v1_8_R1,
-		v1_8_R2,
-		v1_8_R3,
-		v1_9_R1,
-		v1_9_R2
-	}
 
 	private boolean isCompatible() {
 		for (Versions v : Versions.values()) {
@@ -83,16 +75,6 @@ public class InventoryRollback extends JavaPlugin {
 		return false;
 	}
 	
-	public static boolean isV18to19() {
-		for (v18to19 v : v18to19.values()) {
-			if (v.name().equalsIgnoreCase(packageVersion)) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
-
 	public static void startupTasks() {
 		ConfigFile config = new ConfigFile();
 		
--- src/main/java/me/danjono/inventoryrollback/reflections/Packets.java
+++ src/main/java/me/danjono/inventoryrollback/reflections/Packets.java
@@ -1,21 +1,9 @@
 package me.danjono.inventoryrollback.reflections;
 
-import org.bukkit.entity.Player;
-
 import me.danjono.inventoryrollback.InventoryRollback;
 
 public class Packets {
 	
-	public void sendPacket(Player player, Object packet) {
-		try {
-			Object handle = player.getClass().getMethod("getHandle").invoke(player);
-			Object playerConnection = handle.getClass().getField("playerConnection").get(handle);
-			playerConnection.getClass().getMethod("sendPacket", getNMSClass("Packet")).invoke(playerConnection, packet);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 	public Class<?> getNMSClass(String name) {
 		Class<?> c = null;
 		
