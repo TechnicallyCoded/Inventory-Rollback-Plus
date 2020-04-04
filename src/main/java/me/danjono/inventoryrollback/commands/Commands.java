f76d01a859813669aba2908322e779354246fbf9
danjono <12578450+danjono@users.noreply.github.com>
2020-04-04 13:04:14 +0100

    Added tab auto complete to commands

--- src/main/java/me/danjono/inventoryrollback/commands/Commands.java
+++ src/main/java/me/danjono/inventoryrollback/commands/Commands.java
@@ -1,12 +1,20 @@
 package me.danjono.inventoryrollback.commands;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 
 import me.danjono.inventoryrollback.InventoryRollback;
 import me.danjono.inventoryrollback.config.ConfigData;
@@ -18,41 +26,37 @@ import me.danjono.inventoryrollback.gui.menu.MainMenu;
 import me.danjono.inventoryrollback.gui.menu.PlayerMenu;
 import me.danjono.inventoryrollback.inventory.SaveInventory;
 
-public class Commands extends ConfigData implements CommandExecutor {
-
-    private CommandSender sender;
+public class Commands extends ConfigData implements CommandExecutor, TabCompleter {
 
     @Override		
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if(cmd.getName().equalsIgnoreCase("inventoryrollback") || cmd.getName().equalsIgnoreCase("ir")) {
 
-            this.sender = sender;
-
             if (args.length == 0) {
-                restoreCommand(args);
+                restoreCommand(sender, args);
                 return true;
             }
 
             switch (args[0]) {
             case "restore":
-                restoreCommand(args);
+                restoreCommand(sender, args);
                 break;
 
             case "forcebackup":
             case "forcesave":
-                forceBackupCommand(args);
+                forceBackupCommand(sender, args);
                 break;
 
             case "enable":
-                enableCommand();
+                enableCommand(sender);
                 break;
 
             case "disable":
-                disableCommand();
+                disableCommand(sender);
                 break;
 
             case "reload": 
-                reloadCommand();
+                reloadCommand(sender);
                 break;
                 
             case "help":
@@ -60,15 +64,15 @@ public class Commands extends ConfigData implements CommandExecutor {
                 break;
                 
             case "version":
-                sender.sendMessage(MessageData.getPluginName() + "Server is running v" + InventoryRollback.getPluginVersion() + " - Created by danjono");
+                versionCommand(sender);
                 break;
 
             case "convertmysql":
-                convertMySQL();
+                convertMySQL(sender);
                 break;                                
 
             case "convertyaml":
-                convertYAML();
+                convertYAML(sender);
                 break; 
 
             default:
@@ -80,7 +84,7 @@ public class Commands extends ConfigData implements CommandExecutor {
 
     }
 
-    private void restoreCommand(String[] args) {
+    private void restoreCommand(CommandSender sender, String[] args) {
         if (sender instanceof Player) {
             if (sender.hasPermission("inventoryrollback.restore")) {
                 if (!ConfigData.isEnabled()) {
@@ -88,7 +92,7 @@ public class Commands extends ConfigData implements CommandExecutor {
                     return;
                 }
 
-                openBackupMenu((Player) sender, args);
+                openBackupMenu(sender, (Player) sender, args);
                 
             } else {
                 sender.sendMessage(MessageData.getPluginName() + MessageData.getNoPermission());
@@ -98,7 +102,7 @@ public class Commands extends ConfigData implements CommandExecutor {
         }
     }
     
-    private void openBackupMenu(Player staff, String[] args) {
+    private void openBackupMenu(CommandSender sender, Player staff, String[] args) {
         if (args.length <= 0 || args.length == 1) {
             try {
                 openMainMenu(staff);
@@ -129,7 +133,7 @@ public class Commands extends ConfigData implements CommandExecutor {
         Bukkit.getScheduler().runTaskAsynchronously(InventoryRollback.getInstance(), menu::getPlayerMenu);
     }
 
-    private void forceBackupCommand(String[] args) {
+    private void forceBackupCommand(CommandSender sender, String[] args) {
         if (sender.hasPermission("inventoryrollback.forcebackup")) {
             if (args.length == 1 || args.length > 2) {
                 sender.sendMessage(MessageData.getPluginName() + MessageData.getError());
@@ -152,7 +156,7 @@ public class Commands extends ConfigData implements CommandExecutor {
         }
     }
 
-    private void enableCommand() {
+    private void enableCommand(CommandSender sender) {
         if (sender.hasPermission("inventoryrollback.enable")) {
             setEnabled(true);
             saveConfig();
@@ -163,7 +167,7 @@ public class Commands extends ConfigData implements CommandExecutor {
         }
     }
 
-    private void disableCommand() {
+    private void disableCommand(CommandSender sender) {
         if (sender.hasPermission("inventoryrollback.disable")) {
             setEnabled(false);
             saveConfig();
@@ -174,7 +178,7 @@ public class Commands extends ConfigData implements CommandExecutor {
         }
     }
 
-    private void reloadCommand() {
+    private void reloadCommand(CommandSender sender) {
         if (sender.hasPermission("inventoryrollback.reload")) {                                     
             InventoryRollback.startupTasks();
 
@@ -188,16 +192,117 @@ public class Commands extends ConfigData implements CommandExecutor {
         //TODO Write up a help command
     }
     
-    private void convertMySQL() {
+    private void versionCommand(CommandSender sender) {
+        if (sender.hasPermission("inventoryrollback.version"))
+            sender.sendMessage(MessageData.getPluginName() + "Server is running v" + InventoryRollback.getPluginVersion() + " - Created by danjono");
+    }
+    
+    private void convertMySQL(CommandSender sender) {
         if (sender instanceof ConsoleCommandSender && sender.isOp()) {
             Bukkit.getScheduler().runTaskAsynchronously(InventoryRollback.getInstance(), MySQL::convertYAMLToMySQL);
         }
     }
     
-    private void convertYAML() {
+    private void convertYAML(CommandSender sender) {
         if (sender instanceof ConsoleCommandSender && sender.isOp()) {
             Bukkit.getScheduler().runTaskAsynchronously(InventoryRollback.getInstance(), YAML::convertOldBackupData);
         }
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
+        List<String> commands = new ArrayList<>();
+        
+        if (cmd.getName().equalsIgnoreCase("ir") || cmd.getName().equalsIgnoreCase("inventoryrollback")) {
+            switch (args[0]) {
+            case "restore":
+                if (sender.hasPermission("inventoryrollback.restore"))
+                    return restoreAutoComplete(args);
+                break;
+                
+            case "forcebackup":
+            case "forcesave":
+                if (sender.hasPermission("inventoryrollback.forcebackup"))    
+                    return forceBackupAutoComplete(args);
+                break;
+            
+            default:
+                return defaultCommandsAutoComplete(sender, args);
+            }
+        }
+        
+        return commands;
+    }
+    
+    private List<String> defaultCommandsAutoComplete(CommandSender sender, String[] args) {
+        List<String> completions = new ArrayList<>();
+        Set<String> commands = new HashSet<>();
+        
+        if (args.length > 1)
+            return completions;
+        
+        if (sender.hasPermission("inventoryrollback.disable"))
+            commands.add("disable"); 
+        
+        if (sender.hasPermission("inventoryrollback.enable"))
+            commands.add("enable");
+        
+        if (sender.hasPermission("inventoryrollback.forcebackup"))
+            commands.add("forcebackup");
+        
+        if (sender.hasPermission("inventoryrollback.reload"))
+            commands.add("reload");
+        
+        if (sender.hasPermission("inventoryrollback.restore"))
+            commands.add("restore");
+        
+        if (sender.hasPermission("inventoryrollback.version"))
+            commands.add("version");
+        
+        if (!commands.isEmpty())
+            StringUtil.copyPartialMatches(args[0], commands, completions);
+        
+        Collections.sort(completions);
+        return completions;
+    }
+    
+    private List<String> restoreAutoComplete(String[] args) {
+        List<String> commands = new ArrayList<>();
+        
+        if (args.length == 2)
+            commands = playerListAutoComplete();
+        
+        return commands;
+    }
+    
+    private List<String> forceBackupAutoComplete(String[] args) {
+        List<String> completions = new ArrayList<>();
+        Set<String> commands = new HashSet<>();
+        
+        if (args.length == 3 && args[1].equalsIgnoreCase("player"))
+            return playerListAutoComplete();
+        
+        if (args.length == 2) {
+            commands.add("all");
+            commands.add("player");
+        }
+        
+        if (!commands.isEmpty())
+            StringUtil.copyPartialMatches(args[1], commands, completions);
+        
+        Collections.sort(completions);
+        return completions;
+    }
+    
+    private List<String> playerListAutoComplete() {
+        List<String> players = new ArrayList<>();
+        
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            players.add(player.getName());
+        }
+        
+        Collections.sort(players);
+        return players;
     }
 
 }
