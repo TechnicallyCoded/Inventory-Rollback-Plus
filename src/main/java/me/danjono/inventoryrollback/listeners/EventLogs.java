8143f11fbe6519b42ebd4f30c06ab7cf70db2d84
EpicPlayerA10 <adasko.lesiak@gmail.com>
2021-06-21 19:18:35 +0200

    Added checking for cancelled event

--- src/main/java/me/danjono/inventoryrollback/listeners/EventLogs.java
+++ src/main/java/me/danjono/inventoryrollback/listeners/EventLogs.java
@@ -9,7 +9,6 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
