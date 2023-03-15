abdb3cd80a24d7c82e06735891433f062ce4dc8e
TechnicallyCoded <technicallycoded@gmail.com>
2023-03-15 23:41:32 +0100

    Add 1.19.3 and 1.19.4 as supported versions

--- src/main/java/com/nuclyon/technicallycoded/inventoryrollback/nms/EnumNmsVersion.java
+++ src/main/java/com/nuclyon/technicallycoded/inventoryrollback/nms/EnumNmsVersion.java
@@ -20,7 +20,8 @@ public enum EnumNmsVersion {
     v1_18_R1,
     v1_18_R2,
     v1_19_R1,
-    v1_19_R2;
+    v1_19_R2,
+    v1_19_R3;
 
     public boolean isAtLeast(EnumNmsVersion version) {
         return this.ordinal() >= version.ordinal();
